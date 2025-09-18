78fadddf25ff54ebf0c335554c0a578082462428
Udaysuman <udaysuman16@gmail.com>
2025-09-18 18:05:08 +0530

    Initial commit

---
+++ KissanBazzar-deployment/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['newsapi.org', 'www.freep.com'], // Add any other domains you use
+  },
+};
+
+export default nextConfig;
